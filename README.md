06c49b0980a5bfe477d71bfe244c8160441faa41
nonamie <53796817+mathfox@users.noreply.github.com>
2024-10-31 15:36:33 +0300

    refactor runtime

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 This package provides an API for writing systems in function hook-ish style.
 The approach allows for seamless hot-reloading and thus better DX.
 
-Check out the `make-jsx` package which opens the possibility to write UI in this style using JSX.
+Check out the `make-jsx` package which opens the possibility to make a UI in this style using JSX.
 
 ## Loop
 
@@ -15,32 +15,24 @@ It also collects debug information which could be used in debugger implementatio
 ```ts
 const loop = new Loop();
 
-function someSystem_1() {
+function someSystem() {
     // do something with hooks
 }
 
-function someSystem_2() {
-    // do something with hooks
-}
-
-const [step, evict] = loop.withStep(someSystem_1, someSystem_2);
-
+const [step, evict] = loop.scheduleSystem(someSystem);
 const connection = RunService.Heartbeat.Connect(step);
 
-// hot reload stuff
 defineCleanupCallback(() => {
     connection.Disconnect();
+
+    // If you want a cold reload (meaning all of the state will be reset)
+    // you have to evict the system before scheduling the hot reloaded one;
     evict();
 });
 ```
 
-## Builtin hooks
+## Hooks
 
-- useEvent
-- useThrottle
-
-There is `useHookState` function for implementing custom hooks.
-
-## Addons
+Use `useHookState` function for implementing custom hooks.
 
 There is `topo-hooks` package that provides a set of common hooks.
--- src/index.d.ts
+++ src/index.d.ts
@@ -1,38 +1,29 @@
-export type System<TParams extends ReadonlyArray<any> = ReadonlyArray<any>> = (
-	...params: TParams
-) => void;
+export type System = () => void;
 
 export interface FrameState {
 	deltaTime: number;
 }
 
-export interface Node<TParams extends ReadonlyArray<any>> {
+export interface Node {
 	frame?: FrameState;
-	currentSystem?: System<TParams>;
-	system?: Map<
-		System,
-		{
-			storage: Map<string, any>;
-			cleanupCallback: (storage: any) => boolean | void;
-		}
-	>;
+	currentSystem?: System,
+	system?: Map<System, {
+		storage: Map<string, any>;
+		cleanupCallback: (storage: any) => boolean | void;
+	}>;
 }
 
-export function start<TParams extends ReadonlyArray<any>>(
-	node: Node<TParams>,
-	fn: () => void,
-): void;
+export function start(node: Node, fn: () => void): void;
 
 export function useHookState<TStorage>(
+    baseKey: unknown,
 	discriminator?: unknown,
 	cleanupCallback?: (storage: TStorage) => boolean | void,
 ): TStorage;
 
 export function useFrameState(): FrameState;
 
-export function useCurrentSystem<
-	TParams extends ReadonlyArray<any>,
->(): System<TParams>;
+export function useCurrentSystem(): System;
 
 export function withinTopoContext(): boolean;
 
@@ -40,121 +31,32 @@ export interface SystemSamples extends Array<number> {
 	index?: number | undefined;
 }
 
-export function getSystemInfo(system: System): {
-	readonly moduleFullName: string;
-	readonly functionName: string;
-};
-
 /**
  * @class Loop
- * 
- * The main purpose of the Loop is to collect the data about the systems which later on could be used by different debugger implementations.
  *
  * Yielding is not allowed in systems. Doing so will result in the system thread being closed early, but it will not
  * affect other systems.
  */
 export class Loop {
-    profiling?: Map<System, SystemSamples>;
+	_systems: Map<string, System>;
+    _systemState: Map<string, object>;
 
-	_systems: Array<System>;
-    _skipSystems: Set<System>;
+    _skipSystems: Set<string>;
 
 	trackErrors: boolean;
-	_systemErrors: Map<System, {
+	_systemErrors: Map<string, {
 		error: unknown;
 		when: number;
 	}>;
+
+    profiling?: Map<string, SystemSamples>;
 
 	/**
 	 * Schedules a system.
 	 *
 	 * @param system - System to schedule.
 	 */
-	_scheduleSystem(system: System): void;
-
-	/**
-	 * @param system - System to evict from loop.
-	 */
-	_evictSystem(system: System): void;
-
-	/**
-	 * Replaces an older version of a system with a newer version of the system. Internal system storage (which is used
-	 * by hooks) will be moved to be associated with the new system. This is intended to be used for hot reloading.
-	 * @param oldSystem - The old system to be replaced.
-	 * @param newSystem - The new system to replace with.
-	 */
-	_replaceSystem(oldSystem: System, newSystem: System): void;
-
-    /**
-     * Returns the `step` function that should be called each frame.
-     * 
-     * @example
-     * ```ts
-     * function reconcilePlayerCollisions() {
-     *     // some stuff
-     * }
-     * 
-     * const [step, evict] = loop.schedule(reconcilePlayerCollisions)
-     * 
-     * // Custom scheduling
-     * const name = "test";
-     * 
-     * RunService.BindToRenderStep(name, 0, () => {
-     *    step();
-     * })
-     * // when we no longer need the system
-     * RunService.UnbindFromRenderStep(name)
-     * 
-     * // other type of the event
-     * RunService.Heartbeat.Connect(() => {
-     *    step();
-     * })
-     * ```
-     */
-    schedule(...systems: System[]): LuaTuple<[step: () => void, evict: () => void]>;
+	schedule(system: System): LuaTuple<[step: () => void, evict: () => void]>;
 }
 
 export function useDeltaTime(): number;
-
-type ConnectionLike =
-	| { Disconnect(): void }
-	| { disconnect(): void }
-	| { Destroy(): void }
-	| { destroy(): void }
-	| (() => void);
-
-type SignalLike<TArgs extends ReadonlyArray<any>> =
-	| { Connect(callback: (...args: TArgs) => void): ConnectionLike }
-	| { connect(callback: (...args: TArgs) => void): ConnectionLike }
-	| { on(callback: (...args: TArgs) => void): ConnectionLike };
-
-type InferSignalParameters<TValue> = TValue extends SignalLike<infer TParams>
-	? TParams
-	: never;
-
-export function useEvent<
-	TInstance extends Instance,
-	TEvent extends InstanceEventNames<TInstance>,
->(
-	instance: TInstance,
-	event: TEvent,
-): IterableFunction<
-	LuaTuple<
-		[
-			index: number,
-			...rest: InferSignalParameters<InstanceEvents<TInstance>[TEvent]>,
-		]
-	>
->;
-
-export function useEvent<
-	TParams extends ReadonlyArray<any>,
-	TEvent extends SignalLike<TParams>,
->(
-	discriminator: unknown,
-	event: TEvent,
-): IterableFunction<
-	LuaTuple<[index: number, ...rest: InferSignalParameters<TEvent>]>
->;
-
-export function useThrottle(seconds: number, discriminator?: unknown): boolean;
